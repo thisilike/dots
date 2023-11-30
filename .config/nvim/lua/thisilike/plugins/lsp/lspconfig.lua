b4d6e53965db32f2d45d254997eef12e1af9ec59
thisilike <tobias@thisilike.xyz>
2023-11-30 15:31:28 +0100

    added splls language server

--- .config/nvim/lua/thisilike/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/thisilike/plugins/lsp/lspconfig.lua
@@ -165,6 +165,11 @@ return {
       on_attach = on_attach,
     })
 
+    lspconfig["sqlls"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+
     -- configure lua server (with special settings)
     lspconfig["lua_ls"].setup({
       capabilities = capabilities,
